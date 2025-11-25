215496ca0d473490a1c35a9c999f2ce36c30f499
sonavbinu <sonavbinu567@gmail.com>
2025-11-25 10:23:24 +0530

    added push notification api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db";
-import { getEnvVariable } from "./utils/helpers";
-import cookieParser from "cookie-parser";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db';
+import { getEnvVariable } from './utils/helpers';
+import cookieParser from 'cookie-parser';
+import { swaggerSpec, swaggerUiMiddleware } from './swagger';
+import axios from 'axios';
+
+import { error } from 'console';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,25 +15,102 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Middlewares
-app.use(cors({
-  origin: [
-    getEnvVariable('FRONT_END_URL')
-  ],
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: [getEnvVariable('FRONT_END_URL')],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Root 
-app.get("/", async (_req, res) => {
-  res.send("Hai there, API is running...");
-});
+//Swwagger Docs
+app.use(
+  '/api--docs',
+  swaggerUiMiddleware.serve,
+  swaggerUiMiddleware.setup(swaggerSpec)
+);
 
+/**
+ * @swagger
+ * tags:
+ * - name: Notifications
+ *   description:Push Notification Endpoints
+ */
+
+/**
+ * @swagger
+ * /send-notifications :
+ * post:
+ *  summary: Send push notification using Firebase Cloud Messaging
+ *  tags: [Notifications]
+ *  requestBody :
+ *    required:true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       type:object
+ *       properties:
+ *        token:
+ *         type:string
+ *        example:"DEVICE_FCM_TOKEN"
+ *      title:
+ *       type:string
+ *       example:"Hello "
+ *     body:
+ *      type:string
+ *      example:"This is a push notification !"
+ *
+ *  responses:
+ *   200:
+ *    description :Notification sent successfully
+ *   500:
+ *  description :Error sending notification
+ */
+
+app.post('/send-notification', async (req: Request, res: Response) => {
+  const { token, title, body } = req.body;
+
+  if (!token || !title || !body) {
+    return res.status(400).json({
+      error: 'token,title, and body are required',
+    });
+  }
+  try {
+    const response = await axios.post(
+      'https://fcm.googleapis.com/fcm/send',
+      {
+        to: token,
+        notification: { title, body },
+      },
+      {
+        headers: {
+          Authorization: `key=${getEnvVariable('FCM_SERVER_KEY')}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return res.status(200).json({
+      message: 'Notification sent!',
+      firebase_response: response.data,
+    });
+  } catch (error: any) {
+    console.error(error.response?.data || error.message);
+    return res.status(500).json({
+      error: 'Notification failed',
+      details: error.response?.data || error.message,
+    });
+  }
+});
+// Root
+app.get('/', async (_req, res) => {
+  res.send('Hai there, API is running...');
+});
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Swagger Docs -> http://localhost:${PORT}/api-docs`);
 });
---
+++ src/swagger.ts
@@ -0,0 +1,20 @@
+import { version } from 'os';
+import { title } from 'process';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Push Notification API',
+      version: '1.0.0',
+      description:
+        'API to send push notification using Firebase Cloud Messaging',
+    },
+  },
+  apis: ['./src/index.ts'],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
+export const swaggerUiMiddleware = swaggerUi;
